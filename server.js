385f773944bdbb8ea9be450f0a9368f2c5b6ed28
Evan-CwC <evancwc0305@gmail.com>
2024-07-26 16:05:25 +1000

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
-var express = require("express");
-var app = express();
-var port = process.env.port || 3000;
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const addTwoNumbers = (n1, n2) => {
+  return n1 + n2;
+};
+
+app.get("/addTwoNumbers", (req, res) => {
+  const n1 = parseInt(req.query.n1);
+  const n2 = parseInt(req.query.n2);
+
+  if (isNaN(n1) || isNaN(n2)) {
+    return res.status(400).json({ statusCode: 400, message: "Invalid input" });
+  }
+
+  const result = addTwoNumbers(n1, n2);
+  res.json({ statusCode: 200, data: result });
+});
+
 app.listen(port, () => {
-  console.log("App listening to: " + port);
+  console.log(`Server is running on http://localhost:${port}`);
 });
